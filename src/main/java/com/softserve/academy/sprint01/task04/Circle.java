b4542bedf5ba766a7ffc17426201fa7cf86f3e82
Nadiya Fomenko <47325620+nadiyafomenko@users.noreply.github.com>
2020-09-13 18:00:27 +0300

    Initial project structure and sprint-01 tasks

---
+++ src/main/java/com/softserve/academy/sprint01/task04/Circle.java
@@ -0,0 +1,25 @@
+package com.softserve.academy.sprint01.task04;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public void draw() {
+
+    }
+
+    public void draw(String color) {
+
+    }
+
+    public void draw(float scale) {
+
+    }
+
+    public void draw(String color, float scale) {
+
+    }
+}
