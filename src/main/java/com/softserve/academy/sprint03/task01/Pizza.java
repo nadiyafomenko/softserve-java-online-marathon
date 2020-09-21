27edff0df1e931e19325cde34c495c3968316b19
Nadiya Fomenko <47325620+nadiyafomenko@users.noreply.github.com>
2020-09-21 21:33:08 +0300

    sprint 03 changes

--- src/main/java/com/softserve/academy/sprint03/task01/Pizza.java
+++ src/main/java/com/softserve/academy/sprint03/task01/Pizza.java
@@ -1,13 +1,14 @@
 package com.softserve.academy.sprint03.task01;
 
 public class Pizza {
-    private static String cheese;
-    private static String meat;
-    private static String seafood;
-    private static String vegetable;
-    private static String mushroom;
+    private String cheese;
+    private String meat;
+    private String seafood;
+    private String vegetable;
+    private String mushroom;
 
-    private Pizza() { }
+    private Pizza() {
+    }
 
     public String getCheese() {
         return cheese;
@@ -16,14 +17,28 @@ public class Pizza {
     public String getMeat() {
         return meat;
     }
+
     public String getSeafood() {
         return seafood;
     }
+
     public String getVegetable() {
         return vegetable;
     }
+
     public String getMushroom() {
         return mushroom;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "cheese='" + cheese + '\'' +
+                ", meat='" + meat + '\'' +
+                ", seafood='" + seafood + '\'' +
+                ", vegetable='" + vegetable + '\'' +
+                ", mushroom='" + mushroom + '\'' +
+                '}';
     }
 
     public static PizzaBuilder base() {
@@ -32,31 +47,36 @@ public class Pizza {
 
     public static class PizzaBuilder {
         Pizza pizza;
-        private PizzaBuilder() { }
 
-        public PizzaBuilder addCheese(String cheese){
-            Pizza.cheese = cheese;
-            return this;
+        private PizzaBuilder() {
         }
 
-        public PizzaBuilder addMeat(String meat){
-            Pizza.meat = meat;
+        public PizzaBuilder addCheese(String cheese) {
+            pizza.cheese = cheese;
             return this;
         }
 
-        public PizzaBuilder addSeafood(String seafood){
-            Pizza.seafood = seafood;
+        public PizzaBuilder addMeat(String meat) {
+            pizza.meat = meat;
             return this;
         }
-        public PizzaBuilder addVegetable(String vegetable){
-            Pizza.vegetable = vegetable;
+
+        public PizzaBuilder addSeafood(String seafood) {
+            pizza.seafood = seafood;
             return this;
         }
-        public PizzaBuilder addMushroom(String mushroom){
-            Pizza.mushroom = mushroom;
+
+        public PizzaBuilder addVegetable(String vegetable) {
+            pizza.vegetable = vegetable;
             return this;
         }
-        public Pizza build(){
+
+        public PizzaBuilder addMushroom(String mushroom) {
+            pizza.mushroom = mushroom;
+            return this;
+        }
+
+        public Pizza build() {
             return new Pizza();
         }
     }
--- src/main/java/com/softserve/academy/sprint03/task05/ClientType.java
+++ src/main/java/com/softserve/academy/sprint03/task05/ClientType.java
@@ -1,19 +1,36 @@
 package com.softserve.academy.sprint03.task05;
 
 public enum ClientType {
-    NEW(1), SILVER(12), GOLD(30), PLATINUM(60);
+    NEW(1) {
+        @Override
+        public double discount() {
+            return 0.1;
+        }
+    },
+    SILVER(12) {
+        @Override
+        public double discount() {
+            return (100 - 12 * 0.25) / 100;
+        }
+    },
+    GOLD(30) {
+        @Override
+        public double discount() {
+            return (100 - 12 * 0.3) / 100;
+        }
+    },
+    PLATINUM(60) {
+        @Override
+        public double discount() {
+            return (100 - 12 * 0.35) / 100;
+        }
+    };
 
-    private int months;
+    private final int months;
 
     ClientType(int months) {
         this.months = months;
     }
-    public double discount() {
-        switch (this.months){
-            case 12: return (100 - 12 * 0.25) / 100;
-            case 30: return (100 - 30 * 0.3) / 100;
-            case 60: return (100 - 60 * 0.35) / 100;
-            default: return 0.1;
-        }
-    }
+
+    public abstract double discount();
 }
